2ded82766262be2a601bdfa1c88d242436eb7b6d
paul <paul.torre@richmond.edu>
2018-11-21 16:30:52 -0500

    added efficient solution to 70

--- dcp_70.py
+++ dcp_70.py
@@ -5,4 +5,15 @@ while ans < n:
     if sum([int(c) for c in str(t)]) == 10:
         ans += 1
     t += 1
-print(t-1)
+
+
+'''efficient version'''
+ans = 0
+n = 2
+t = 19
+while ans < n:
+    if sum([int(c) for c in str(t)]) == 10:
+        ans += 1
+    t += 9
+    
+print(t-9) if n > 0 else print(0)
